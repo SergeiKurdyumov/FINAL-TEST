bb10660356724999d8bbf6a8eaa6568598b9e2f3
SergeyKurdumov <apple570@gmail.com>
2023-01-29 21:59:39 +0400

    Add Algorithm

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿ string[] Array(string[] num)
+{
+    var result = new string[num .Length];
+    var index = 0;
+    foreach (var str in num )
+    {
+        if (str.Length <= 3)
+        {
+            result[index] = str;
+            index++;
+        }
+    }
+    return result;
+}
+
+void ShowArray (string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {        
+        Console.Write($"{arr[i]} ");    
+    }
+}
+
+string[] array = {"hellow", "world", "123", "GB", "2"};
+string[] NewMassiv = Array(array);
+ShowArray(NewMassiv);
